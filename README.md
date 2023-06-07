df9a341696b7d6b6bb08ade15f8d34599d880309
Alex Maggio <alexgerman@gmail.com>
2023-06-07 18:59:50 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ProgressUpdate
+
+A description of this package.
---
+++ Sources/ProgressUpdate/ProgressUpdate.swift
@@ -0,0 +1,6 @@
+public struct ProgressUpdate {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
